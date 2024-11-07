d89867b94391c1d6799bd00557934ba5caa9fed5
Gabriel Alberto <gahalberto@icloud.com>
2024-11-07 14:10:31 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
     <>
       <h1 className="text-red-300 p-5 flex">Hello World</h1>
+      <Button>Clique aqui</Button>
     </>
   )
 }
