51aadf2c6cfcd28e02f1ede2ff1b5c691124309f
Gabriel Alberto <gahalberto@icloud.com>
2024-11-07 13:54:10 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
-  return(
-    <>Oie</>
+  return (
+    <>
+      <h1 className="text-red-300 p-5 flex">Hello World</h1>
+    </>
   )
 }
 
-export default Home;
+export default Home;  
