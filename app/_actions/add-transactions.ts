ceb3c0530695162f7e85623f93cb06b75d29e8cc
Gabriel Alberto <gahalberto@icloud.com>
2024-11-18 12:02:32 -0300

    fix: upsert function

--- app/_actions/add-transactions.ts
+++ app/_actions/add-transactions.ts
@@ -9,7 +9,6 @@ import { db } from "../_lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { addTransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
-import { v4 as uuidv4 } from "uuid"; // Biblioteca para gerar UUIDs
 
 interface addTransactionParams {
   id?: string;
@@ -29,15 +28,12 @@ export const upsertTransaction = async (params: addTransactionParams) => {
   if (!userId) {
     throw new Error("Unauthorized");
   }
-  // Garante que `id` sempre tenha um valor
-  const id = params.id || uuidv4();
-
   await db.transaction.upsert({
-    where: {
-      id,
-    },
     update: { ...params, userId },
     create: { ...params, userId },
+    where: {
+      id: params.id ?? "",
+    },
   });
   revalidatePath("/transactions");
 };
